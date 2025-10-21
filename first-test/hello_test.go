51d5216c10510f19a392c16f10b7c1206a096c32
ToxicTuring <toxicturing@pm.me>
2025-10-21 12:24:23 +0200

    Refactored test to use t.Run to add more inclusive tests in the future

--- first-test/hello_test.go
+++ first-test/hello_test.go
@@ -3,10 +3,17 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("Got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
+	})
+
+	t.Run("calling hello with empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+	})
 }
